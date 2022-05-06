f9f17bc028c00ca0c9cee52a554e89354c33ebbb
HalcyonAzure <halc-days@outlook.com>
2022-05-06 21:29:32 +0800

    update ignore directory

--- shell/rclone-backup.sh
+++ shell/rclone-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/bin/tar cvzf "/root/backup/docker-$(date +%F).tar.gz" --exclude='Downloads*' --exclude='bak*' --exclude='log*' /data
+/usr/bin/tar cvzf "/root/backup/docker-$(date +%F).tar.gz" --exclude='Downloads*' --exclude='bak*' --exclude='log*' --exclude='cache*' /data
 /usr/bin/rclone sync --cache-chunk-size=16M --transfers=16 "/root/backup" "1drive:/backup/docker" --progress
 find /root/backup -mtime +30 -name "*.zip" -exec rm -rf {} \;
 echo "BACKUP DATE:" "$(date +"%Y-%m-%d %H:%M:%S") ">> /var/log/backup.log
